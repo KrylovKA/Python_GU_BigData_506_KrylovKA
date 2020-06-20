27746bfa5844c0000328567d67fd72590309285d
KrylovKA <krylovearth@gmail.com>
2020-06-20 14:13:40 +0300

    Create new branch - add README.md,.gitignore - add folder DZ#2

---
+++ DZ#2/Task #2.py
@@ -0,0 +1,18 @@
+# Для списка реализовать обмен значений соседних элементов,
+# т.е.значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
+# При нечетном количестве элементов последний сохранить на своем месте.
+# Для заполнения списка элементов необходимо использовать функцию input().
+
+# Решение:
+elements_count = int(input("Введите количество элементов списка "))
+my_list = []
+i = 0
+element = 0
+while i < elements_count:
+    my_list.append(input("Введите следующее значение списка "))
+    i += 1
+
+for element in range(int(len(my_list)/2)):
+        my_list[element], my_list[element + 1] = my_list [element + 1], my_list[element]
+        element += 2
+print(my_list)
